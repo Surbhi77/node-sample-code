c7e177db4ea8707e9f3424f782b0b76f27c3721d
SURBHI <sur@9806479092@gmail.com>
2024-07-31 20:04:09 +0530

    chnages in server file

--- src/server.ts
+++ src/server.ts
@@ -11,6 +11,8 @@ if (result.error) {
 const mongoConnection = new MongoConnection(process.env.MONGO_URL);
 
 console.log("object")
+console.log("object")
+console.log("object")
 
 if (process.env.MONGO_URL == null) {
   logger.log({
