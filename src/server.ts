03953631b1a0fcb0e3bb47dc6875fe48b666d40c
SURBHI <sur@9806479092@gmail.com>
2024-10-10 21:35:31 +0530

    changes in server file

--- src/server.ts
+++ src/server.ts
@@ -13,8 +13,9 @@ const mongoConnection = new MongoConnection(process.env.MONGO_URL);
 console.log("object")
 console.log("object")
 console.log("object")
-console.log("object")
 
+console.log("object")
+console.log("object")
 
 if (process.env.MONGO_URL == null) {
   logger.log({
